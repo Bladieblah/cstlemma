a0fed9e9cb24ec5d24dfb62373acd6323029d2e7
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-06-21 11:57:27 +0200

    revert

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import Extension, setup
 
-import os
-
-cwd = os.getcwd()
 
 # Definition of extension modules
 pycstlemma = Extension('cLemmatiser',
@@ -48,10 +45,10 @@ pycstlemma = Extension('cLemmatiser',
                     'src/parsesgml/src/parsesgml.cpp',
                 ],
                  include_dirs = [
-                    os.path.join(cwd, 'src/cstlemma/src'),
-                    os.path.join(cwd, 'src/hashmap/src'),
-                    os.path.join(cwd, 'src/letterfunc/src'),
-                    os.path.join(cwd, 'src/parsesgml/src'),
+                    'src/cstlemma/src',
+                    'src/hashmap/src',
+                    'src/letterfunc/src',
+                    'src/parsesgml/src',
                 ],
                 extra_compile_args=[
                     '-std=c++11',
